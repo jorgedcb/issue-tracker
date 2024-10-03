1e660ce003a96ecb2082b15510ad1fe151be7644
Jorge <jecb2001@gmail.com>
2024-10-02 20:58:07 -0500

    Handle form submition

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,15 +1,8 @@
-// postcss.config.mjs
-import postcssImport from 'postcss-import';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [
-    postcssImport,
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: {
+    tailwindcss: {},
+  },
 };
 
 export default config;
