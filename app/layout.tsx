f11dfa54e032b634925323e4251255d5c2952075
Jorge <jecb2001@gmail.com>
2024-10-02 19:50:53 -0500

    customize Radix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
-import "@radix-ui/themes/styles.css";
-import "./globals.css";
+import "./styles.css";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
-import localFont from "next/font/local";
 import NavBar from "./NavBar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -28,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.variable}>
         <Theme>
           <NavBar />
           <main className="p-5">{children}</main>
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,15 @@
+// postcss.config.mjs
+import postcssImport from 'postcss-import';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+  plugins: [
+    postcssImport,
+    tailwindcss,
+    autoprefixer,
+  ],
 };
 
 export default config;
