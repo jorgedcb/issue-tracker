61d74f4ddb2f0dcede53a2b8203a715e6da3ea56
Jorge <jecb2001@gmail.com>
2024-10-19 21:52:34 -0500

    Improve the loading experience

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,13 +1,3 @@
-import { Skeleton } from "@/app/componets";
-import { Box } from "@radix-ui/themes";
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
 
-const LoadingNewIssuesPage = () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton/>
-      <Skeleton height="20rem" />
-    </Box>
-  );
-};
-
-export default LoadingNewIssuesPage;
+export default IssueFormSkeleton;
